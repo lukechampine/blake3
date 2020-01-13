f87940e24599010158324ef465e4294cdedfd510
lukechampine <luke.champine@gmail.com>
2020-01-13 13:53:16 -0500

    add compress comments, remove gen

--- blake3.go
+++ blake3.go
@@ -77,108 +77,90 @@ type node struct {
 }
 
 // compress is the core hash function, generating 16 pseudorandom words from a
-// node. When nodes are being merged into parents, only the first 8 words are
-// used. When the root node is being used to generate output, the full 16 words
-// are used.
-func (n node) compress() (state [16]uint32) {
-	// round1 rather than init s and mix, do both.
-	// Mix the columns.
+// node.
+func (n node) compress() [16]uint32 {
+	// NOTE: we unroll all of the rounds, as well as the permutations that occur
+	// between rounds.
+
+	// round 1 (also initializes state)
+	// columns
 	s0, s4, s8, s12 := g(n.cv[0], n.cv[4], iv[0], uint32(n.counter), n.block[0], n.block[1])
 	s1, s5, s9, s13 := g(n.cv[1], n.cv[5], iv[1], uint32(n.counter>>32), n.block[2], n.block[3])
 	s2, s6, s10, s14 := g(n.cv[2], n.cv[6], iv[2], n.blockLen, n.block[4], n.block[5])
 	s3, s7, s11, s15 := g(n.cv[3], n.cv[7], iv[3], n.flags, n.block[6], n.block[7])
-
-	// Mix the diagonals.
+	// diagonals
 	s0, s5, s10, s15 = g(s0, s5, s10, s15, n.block[8], n.block[9])
 	s1, s6, s11, s12 = g(s1, s6, s11, s12, n.block[10], n.block[11])
 	s2, s7, s8, s13 = g(s2, s7, s8, s13, n.block[12], n.block[13])
 	s3, s4, s9, s14 = g(s3, s4, s9, s14, n.block[14], n.block[15])
 
 	// round 2
-	// Mix the columns.
 	s0, s4, s8, s12 = g(s0, s4, s8, s12, n.block[2], n.block[6])
 	s1, s5, s9, s13 = g(s1, s5, s9, s13, n.block[3], n.block[10])
 	s2, s6, s10, s14 = g(s2, s6, s10, s14, n.block[7], n.block[0])
 	s3, s7, s11, s15 = g(s3, s7, s11, s15, n.block[4], n.block[13])
-
-	// Mix the diagonals.
 	s0, s5, s10, s15 = g(s0, s5, s10, s15, n.block[1], n.block[11])
 	s1, s6, s11, s12 = g(s1, s6, s11, s12, n.block[12], n.block[5])
 	s2, s7, s8, s13 = g(s2, s7, s8, s13, n.block[9], n.block[14])
 	s3, s4, s9, s14 = g(s3, s4, s9, s14, n.block[15], n.block[8])
 
 	// round 3
-	// Mix the columns.
 	s0, s4, s8, s12 = g(s0, s4, s8, s12, n.block[3], n.block[4])
 	s1, s5, s9, s13 = g(s1, s5, s9, s13, n.block[10], n.block[12])
 	s2, s6, s10, s14 = g(s2, s6, s10, s14, n.block[13], n.block[2])
 	s3, s7, s11, s15 = g(s3, s7, s11, s15, n.block[7], n.block[14])
-
-	// Mix the diagonals.
 	s0, s5, s10, s15 = g(s0, s5, s10, s15, n.block[6], n.block[5])
 	s1, s6, s11, s12 = g(s1, s6, s11, s12, n.block[9], n.block[0])
 	s2, s7, s8, s13 = g(s2, s7, s8, s13, n.block[11], n.block[15])
 	s3, s4, s9, s14 = g(s3, s4, s9, s14, n.block[8], n.block[1])
 
 	// round 4
-	// Mix the columns.
 	s0, s4, s8, s12 = g(s0, s4, s8, s12, n.block[10], n.block[7])
 	s1, s5, s9, s13 = g(s1, s5, s9, s13, n.block[12], n.block[9])
 	s2, s6, s10, s14 = g(s2, s6, s10, s14, n.block[14], n.block[3])
 	s3, s7, s11, s15 = g(s3, s7, s11, s15, n.block[13], n.block[15])
-
-	// Mix the diagonals.
 	s0, s5, s10, s15 = g(s0, s5, s10, s15, n.block[4], n.block[0])
 	s1, s6, s11, s12 = g(s1, s6, s11, s12, n.block[11], n.block[2])
 	s2, s7, s8, s13 = g(s2, s7, s8, s13, n.block[5], n.block[8])
 	s3, s4, s9, s14 = g(s3, s4, s9, s14, n.block[1], n.block[6])
 
 	// round 5
-	// Mix the columns.
 	s0, s4, s8, s12 = g(s0, s4, s8, s12, n.block[12], n.block[13])
 	s1, s5, s9, s13 = g(s1, s5, s9, s13, n.block[9], n.block[11])
 	s2, s6, s10, s14 = g(s2, s6, s10, s14, n.block[15], n.block[10])
 	s3, s7, s11, s15 = g(s3, s7, s11, s15, n.block[14], n.block[8])
-
-	// Mix the diagonals.
 	s0, s5, s10, s15 = g(s0, s5, s10, s15, n.block[7], n.block[2])
 	s1, s6, s11, s12 = g(s1, s6, s11, s12, n.block[5], n.block[3])
 	s2, s7, s8, s13 = g(s2, s7, s8, s13, n.block[0], n.block[1])
 	s3, s4, s9, s14 = g(s3, s4, s9, s14, n.block[6], n.block[4])
 
 	// round 6
-	// Mix the columns.
 	s0, s4, s8, s12 = g(s0, s4, s8, s12, n.block[9], n.block[14])
 	s1, s5, s9, s13 = g(s1, s5, s9, s13, n.block[11], n.block[5])
 	s2, s6, s10, s14 = g(s2, s6, s10, s14, n.block[8], n.block[12])
 	s3, s7, s11, s15 = g(s3, s7, s11, s15, n.block[15], n.block[1])
-
-	// Mix the diagonals.
 	s0, s5, s10, s15 = g(s0, s5, s10, s15, n.block[13], n.block[3])
 	s1, s6, s11, s12 = g(s1, s6, s11, s12, n.block[0], n.block[10])
 	s2, s7, s8, s13 = g(s2, s7, s8, s13, n.block[2], n.block[6])
 	s3, s4, s9, s14 = g(s3, s4, s9, s14, n.block[4], n.block[7])
 
 	// round 7
-	// Mix the columns.
 	s0, s4, s8, s12 = g(s0, s4, s8, s12, n.block[11], n.block[15])
 	s1, s5, s9, s13 = g(s1, s5, s9, s13, n.block[5], n.block[0])
 	s2, s6, s10, s14 = g(s2, s6, s10, s14, n.block[1], n.block[9])
 	s3, s7, s11, s15 = g(s3, s7, s11, s15, n.block[8], n.block[6])
-
-	// Mix the diagonals.
 	s0, s5, s10, s15 = g(s0, s5, s10, s15, n.block[14], n.block[10])
 	s1, s6, s11, s12 = g(s1, s6, s11, s12, n.block[2], n.block[12])
 	s2, s7, s8, s13 = g(s2, s7, s8, s13, n.block[3], n.block[4])
 	s3, s4, s9, s14 = g(s3, s4, s9, s14, n.block[7], n.block[13])
 
-	state = [16]uint32{
+	// finalization
+	return [16]uint32{
 		s0 ^ s8, s1 ^ s9, s2 ^ s10, s3 ^ s11,
 		s4 ^ s12, s5 ^ s13, s6 ^ s14, s7 ^ s15,
 		s8 ^ n.cv[0], s9 ^ n.cv[1], s10 ^ n.cv[2], s11 ^ n.cv[3],
 		s12 ^ n.cv[4], s13 ^ n.cv[5], s14 ^ n.cv[6], s15 ^ n.cv[7],
 	}
-	return
 }
 
 // chainingValue returns the first 8 words of the compressed node. This is used
