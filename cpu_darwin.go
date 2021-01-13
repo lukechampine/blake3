d373079f3544b3e36fab38e3ccc3df6ca3fb004f
Luke Champine <luke.champine@gmail.com>
2021-01-13 12:11:26 -0500

    Prevent panic in darwin AVX512 detection

--- cpu_darwin.go
+++ cpu_darwin.go
@@ -17,6 +17,6 @@ func init() {
 	if !haveAVX512 {
 		// On some Macs, AVX512 detection is buggy, so fallback to sysctl
 		b, _ := syscall.Sysctl("hw.optional.avx512f")
-		haveAVX512 = b[0] == 1
+		haveAVX512 = len(b) > 0 && b[0] == 1
 	}
 }
