85fa20df84c15080ad9de2fdf877be6f32bf9c56
lukechampine <luke.champine@gmail.com>
2023-05-15 15:13:48 -0400

    set digest size of defaultHasher
    Technically, this size is never used, but setting it to 0 has caused
    confusion for people reading the source code.

--- blake3.go
+++ blake3.go
@@ -176,7 +176,7 @@ func New(size int, key []byte) *Hasher {
 
 // Sum256 and Sum512 always use the same hasher state, so we can save some time
 // when hashing small inputs by constructing the hasher ahead of time.
-var defaultHasher = New(0, nil)
+var defaultHasher = New(64, nil)
 
 // Sum256 returns the unkeyed BLAKE3 hash of b, truncated to 256 bits.
 func Sum256(b []byte) (out [32]byte) {
@@ -206,11 +206,11 @@ func Sum512(b []byte) (out [64]byte) {
 // DeriveKey derives a subkey from ctx and srcKey. ctx should be hardcoded,
 // globally unique, and application-specific. A good format for ctx strings is:
 //
-//    [application] [commit timestamp] [purpose]
+//	[application] [commit timestamp] [purpose]
 //
 // e.g.:
 //
-//    example.com 2019-12-25 16:18:03 session tokens v1
+//	example.com 2019-12-25 16:18:03 session tokens v1
 //
 // The purpose of these requirements is to ensure that an attacker cannot trick
 // two different applications into using the same context string.
